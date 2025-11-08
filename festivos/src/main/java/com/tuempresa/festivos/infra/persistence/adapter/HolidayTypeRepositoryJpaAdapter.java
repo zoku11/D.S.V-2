ca16d4e9bd88b840e1f2ea023149331c1e994981
zoku11 <pablocuartas123@gmail.com>
2025-11-07 20:01:53 -0500

    commit 2 3

--- festivos/src/main/java/com/tuempresa/festivos/infra/persistence/adapter/HolidayTypeRepositoryJpaAdapter.java
+++ festivos/src/main/java/com/tuempresa/festivos/infra/persistence/adapter/HolidayTypeRepositoryJpaAdapter.java
@@ -1,17 +1,35 @@
+package com.tuempresa.festivos.infra.persistence.adapter;
+
+import org.springframework.stereotype.Component;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import com.tuempresa.festivos.infra.persistence.repo.HolidayTypeJpaRepository;
+import com.tuempresa.festivos.infra.persistence.entity.HolidayTypeEntity;
+
 @Component
-public class CountryRepositoryJpaAdapter implements com.tuempresa.festivos.domain.port.repository.CountryRepository {
-    private final CountryJpaRepository repo;
-    public CountryRepositoryJpaAdapter(CountryJpaRepository repo){ this.repo = repo; }
-    @Override public com.tuempresa.festivos.domain.model.Country save(com.tuempresa.festivos.domain.model.Country c){ 
-        CountryEntity e = CountryEntity.fromDomain(c);
-        CountryEntity s = repo.save(e);
+public class HolidayTypeRepositoryJpaAdapter implements com.tuempresa.festivos.domain.port.repository.HolidayTypeRepository {
+    private final HolidayTypeJpaRepository repo;
+
+    public HolidayTypeRepositoryJpaAdapter(HolidayTypeJpaRepository repo){ this.repo = repo; }
+
+    @Override
+    public com.tuempresa.festivos.domain.model.HolidayType save(com.tuempresa.festivos.domain.model.HolidayType ht){
+        HolidayTypeEntity e = HolidayTypeEntity.fromDomain(ht);
+        HolidayTypeEntity s = repo.save(e);
         return s.toDomain();
     }
-    @Override public Optional<com.tuempresa.festivos.domain.model.Country> findById(Long id){
-        return repo.findById(id).map(CountryEntity::toDomain);
+
+    @Override
+    public Optional<com.tuempresa.festivos.domain.model.HolidayType> findById(Integer id){
+        return repo.findById(id).map(HolidayTypeEntity::toDomain);
     }
-    @Override public List<com.tuempresa.festivos.domain.model.Country> findAll(){
-        return repo.findAll().stream().map(CountryEntity::toDomain).collect(Collectors.toList());
+
+    @Override
+    public List<com.tuempresa.festivos.domain.model.HolidayType> findAll(){
+        return repo.findAll().stream().map(HolidayTypeEntity::toDomain).collect(Collectors.toList());
     }
-    @Override public void deleteById(Long id){ repo.deleteById(id); }
+
+    @Override
+    public void deleteById(Integer id){ repo.deleteById(id); }
 }
